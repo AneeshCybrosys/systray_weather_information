50441f41807c8dc5e87745542ba4bee177da330e
AneeshCybrosys <aneeshmathapuzha@gmail.com>
2022-08-31 08:37:49 +0530

    aug 31.8.37

--- static/src/js/weather.js
+++ static/src/js/weather.js
@@ -4,42 +4,47 @@ var core = require('web.core');
 var SystrayMenu = require('web.SystrayMenu');
 var Widget = require('web.Widget');
 var QWeb = core.qweb;
-var lon;
-var lat;
 var ExampleWidget = Widget.extend({
    template: 'WeatherInformationSystray',
    events: {
        'click .dropdown-toggle': '_onClick',
    },
-//   onload = (event) => {
-//   console.log("loadd")
-//   };
    _onClick: function(){
-         console.log("kjdsakjd")
-         const API = "2fef88c2a34ff37a65656175a709fb22";
-         window.addEventListener("load", () => {
-//         if (navigator.geolocation) {
-//            navigator.geolocation.getCurrentPosition((position) => {
-//            console.log(position,"popaosp");
-//            }
-//            }
-         });
-//            navigator.geolocation.getCurrentPosition((position) => {
-//            console.log(position,"position");
-//            lon = position.coords.longitude;
-//            lat = position.coords.latitude;
-//            }
-//            }
-//         const base = `https://openweathermap.org/weather?&appid=2fef88c2a34ff37a65656175a709fb22`;
-//         fetch(base).then((response) => {
-//          return response.json();
-//        }).then((data) => {
-//            console.log(data,"lskji")
-//        });
-         var weather = QWeb.render("weather_details", {widget:self,
-            date_now:'jkhkj'
-         });
-         $('.weather_notification').html(weather);
+         if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                var long = position.coords.longitude;
+                var lat = position.coords.latitude;
+                var urlWeatherApi = "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid=2fef88c2a34ff37a65656175a709fb22";
+                fetch(urlWeatherApi)
+                    .then((response) => {
+                      return response.json();
+                    })
+                    .then((data) => {
+                    console.log(data.weather[0]['icon'],"data")
+                    var currentdate = new Date();
+                    var datetime = "Last Updated: " + currentdate.getDate() + "/"
+                        + (currentdate.getMonth()+1)  + "/"
+                        + currentdate.getFullYear() + " "
+                        + currentdate.getHours() + ":"
+                        + currentdate.getMinutes() + ":"
+                        + currentdate.getSeconds();
+                    var temp = Math.round(data.main['temp']-273)
+                    var max_temp = Math.round(data.main['temp_max']-273)
+                    var min_temp = Math.round(data.main['temp_min']-273)
+                    var weather = QWeb.render("weather_details", {widget:self,
+                        date_now:datetime,
+                        max_temp:max_temp,
+                        min_temp:min_temp,
+                        temp:temp,
+                        name:data.name,
+                        weather:data.weather[0]['main'],
+//                        icon:"http://openweathermap.org/img/wn/+data.weather[0]['icon']@2x.png+",
+                        description:data.weather[0]['description']
+                        });
+                        $('.weather_notification').html(weather);
+                    });
+            });
+         }
    },
 });
 SystrayMenu.Items.push(ExampleWidget);
